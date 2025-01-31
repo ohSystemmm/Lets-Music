b9b7cc20e01bf0200b2070929fb4eade55dea4a6
Y2kun <me@y2kun.dev>
2025-01-31 08:55:22 +0100

    added a whole bunch of comments, and lenghtend the testing elements

--- TUI/App.go
+++ TUI/App.go
@@ -10,13 +10,16 @@ import (
 	bzone "github.com/lrstanley/bubblezone"
 )
 
+// Defines variables that can be used via model, oftentimes
 type model struct {
-	table  table.Model
+	list   table.Model
 	width  int
 	height int
 }
 
+// The List function that defines the properties of the table
 func List() model {
+	// the Columns + the tiles + the width, more should probably not be explained
 	columns := []table.Column{
 		{Title: "Title", Width: 40},
 		// {Title: "Category", Width: 8},
@@ -38,7 +41,30 @@ func List() model {
 	// 	{"1 2 Name", "Movie", "92/100", "2018", "2018", "Finished", "Beautiful and emotional."},
 	// 	{"1 2 Slayer", "Anime", "95/100", "2019", "Ongoing", "Ongoing", "Incredible animation."},
 	// }
+
+	// these should be in the future filled automaticly by the backend, and updated when neccesary
 	rows := []table.Row{
+		{"ASD"},
+		{"asdwqr"},
+		{"rfew"},
+		{"fgdgs"},
+		{"qwdsa"},
+		{"frsd"},
+		{"h56a"},
+		{"ASD"},
+		{"asdwqr"},
+		{"rfew"},
+		{"fgdgs"},
+		{"qwdsa"},
+		{"frsd"},
+		{"h56a"},
+		{"ASD"},
+		{"asdwqr"},
+		{"rfew"},
+		{"fgdgs"},
+		{"qwdsa"},
+		{"frsd"},
+		{"h56a"},
 		{"ASD"},
 		{"asdwqr"},
 		{"rfew"},
@@ -48,18 +74,21 @@ func List() model {
 		{"h56a"},
 	}
 
+	// here the configuration of the table gets applied
 	t := table.New(
 		table.WithColumns(columns),
 		table.WithRows(rows),
 		table.WithFocused(true),
-		table.WithHeight(32),
+		table.WithHeight(2),
 	)
 
+	// here the style gets applied
 	t.SetStyles(defineTableStyles())
 
-	return model{table: t}
+	return model{list: t}
 }
 
+// the style of the table, done via lipgloss
 func defineTableStyles() table.Styles {
 	styles := table.DefaultStyles()
 	styles.Selected = styles.Selected.
@@ -70,56 +99,69 @@ func defineTableStyles() table.Styles {
 	return styles
 }
 
+// the function run in the main.go to start the TUI
 func Run() {
 	bzone.NewGlobal()
+	// the bubbletea "Heart", here the properties of the TUI get defined and the Modules get included
 	p := tea.NewProgram(
 		List(),
 		tea.WithAltScreen(),
 		tea.WithMouseCellMotion(),
 		tea.WithMouseAllMotion(),
 	)
+	// WHen an Error occures, exits the program
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
 	}
 }
 
+// Runs when the Bubbletea gets initialized
 func (m model) Init() tea.Cmd {
 	return tea.Batch(
-		tea.SetWindowTitle("Ani-Track"),
+		// Sets the TItle to MElodex, probably no changes needed in the future
+		tea.SetWindowTitle("Melodex"),
 	)
 }
 
+// the update function that reapeats its actions, duh
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 
+	// thist switch is to listen to msgs, they are like events that occure and there are many of them, they get defined in the docs
 	switch msg := msg.(type) {
+	// listens for keypresses
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "q":
 			return m, tea.Quit
 		case "up":
-			m.table.MoveUp(1)
+			m.list.MoveUp(1)
 		case "down":
-			m.table.MoveDown(1)
+			m.list.MoveDown(1)
 		}
 
+	// listens for the mouse
 	case tea.MouseMsg:
 		if msg.Type == tea.MouseLeft {
-			if msg.Y >= 2 && msg.Y < (2+m.table.Height()) {
+			// thi9 is currently to set the row in the list via mouse when to lazy for keys
+			if msg.Y >= 2 && msg.Y < (2+m.list.Height()) {
 				rowIdx := msg.Y - 2
-				m.table.SetCursor(rowIdx)
+				m.list.SetCursor(rowIdx)
 			}
 		}
 
+	// Sets the Size of the window, via msgs
 	case tea.WindowSizeMsg:
 		m.width, m.height = msg.Width, msg.Height
-		m.table.SetHeight((m.height / 2) - 3)
+		// the tableheight
+		m.list.SetHeight((m.height / 1) - 2)
 	}
 
 	return m, cmd
 }
 
+// this is waht ultimatly decides gets to be applied to the output, via the view of the modules
 func (m model) View() string {
-	return lipgloss.NewStyle().BorderStyle(lipgloss.RoundedBorder()).Render(m.table.View())
+	return lipgloss.NewStyle().BorderStyle(lipgloss.RoundedBorder()).Render(m.list.View())
 }
