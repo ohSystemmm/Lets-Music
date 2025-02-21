0c683a297404aa1cec614497d6322c3c2896cd16
システム君 <leonhardfresacher@gmail.com>
2025-02-21 09:59:00 +0100

    Playlisting

--- Backend/Backend.go
+++ Backend/Backend.go
@@ -1,1 +1,61 @@
 package Backend
+
+import (
+	"Melodex/Backend/Music"
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/hajimehoshi/ebiten/v2/audio"
+)
+
+func Backend() {
+	songDir := "./Backend/songs"
+
+	files, err := ioutil.ReadDir(songDir)
+	if err != nil {
+		log.Fatalf("Error reading directory: %v", err)
+	}
+
+	audioContext := audio.NewContext(44100)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		filePath := songDir + "/" + file.Name()
+		fmt.Printf("Playing: %s\n", filePath)
+
+		err := Music.PlaySong(filePath, audioContext)
+		if err != nil {
+			log.Fatalf("Error playing song: %v", err)
+		}
+
+		for {
+			fmt.Println("Enter command (p: pause/resume, s: stop, n: next song):")
+			input, _ := reader.ReadString('\n')
+
+			switch input {
+			case "p\n":
+				Music.PauseSong()
+			case "s\n":
+				Music.StopSong()
+				break
+			case "n\n":
+				Music.StopSong()
+				break
+			default:
+				fmt.Println("Invalid command")
+			}
+
+			if input == "s\n" || input == "n\n" {
+				break
+			}
+		}
+	}
+}
--- Backend/Playlist/PlaylistHandler.go
+++ Backend/Playlist/PlaylistHandler.go
@@ -1,1 +1,9 @@
 package Playlist
+
+const originalPlaylistPath = "$HOME/.local/share/melodex"
+
+func CopyPlaylist()       {} // Copies an existing, defined Playlist to .local/
+func UpdatePlaylist()     {}
+func CreatePlaylist()     {}
+func DeletePlaylist()     {}
+func GetPlaylistContent() {}
